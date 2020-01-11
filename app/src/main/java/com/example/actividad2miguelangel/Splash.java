d510c3c903a27183a14541d7bdf2979cdfeac41b
Miguel Angel <Miguelangelnunezlopez96@gmail.com>
2020-01-11 14:27:11 +0100

    Login

--- app/src/main/java/com/example/actividad2miguelangel/Splash.java
+++ app/src/main/java/com/example/actividad2miguelangel/Splash.java
@@ -2,13 +2,14 @@ package com.example.actividad2miguelangel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
-public class Splash extends AppCompatActivity {
+public class Splash extends AppCompatActivity implements Animation.AnimationListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +24,26 @@ public class Splash extends AppCompatActivity {
         titulo.setTypeface(miFuente);
 
         Animation anim = AnimationUtils.loadAnimation(this,R.anim.animacion);
+        titulo.startAnimation(anim);
+        anim.setAnimationListener(this);
+
+    }
+
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
     }
 }
