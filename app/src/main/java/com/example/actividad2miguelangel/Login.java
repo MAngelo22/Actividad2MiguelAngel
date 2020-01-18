93411545051273f2b30ac081107c79783ac343cf
Miguel Angel <Miguelangelnunezlopez96@gmail.com>
2020-01-18 22:23:04 +0100

    Login acabado

--- app/src/main/java/com/example/actividad2miguelangel/Login.java
+++ app/src/main/java/com/example/actividad2miguelangel/Login.java
@@ -3,14 +3,23 @@ package com.example.actividad2miguelangel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.actividad2miguelangel.TaskTable.TaskContract;
+import com.example.actividad2miguelangel.UsersTable.UserDbHelper;
+import com.example.actividad2miguelangel.UsersTable.UsersContract;
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
 
 public class Login extends AppCompatActivity implements Animation.AnimationListener {
 
@@ -23,15 +32,15 @@ public class Login extends AppCompatActivity implements Animation.AnimationListe
         final TextView textPassword = (TextView) findViewById(R.id.textPas);
 
         //Aqui hago un onclick escuchando a la funcion de vaciar
-        textName.setOnClickListener(new View.OnClickListener(){
+        textName.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View vista){
+            public void onClick(View vista) {
                 vaciar(textName);
             }
         });
-        textPassword.setOnClickListener(new View.OnClickListener(){
+        textPassword.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View vista){
+            public void onClick(View vista) {
                 vaciar(textPassword);
             }
         });
@@ -39,34 +48,56 @@ public class Login extends AppCompatActivity implements Animation.AnimationListe
         getSupportActionBar().hide();
 
         TextView titulo2 = (TextView) findViewById(R.id.text2);
-     }
+    }
 
-    public void crearUsuario (View view){
-
-        Intent cambioUs = new Intent (this, LoginNew.class);
+    public void crearUsuario(View view) {
+        Intent cambioUs = new Intent(this, LoginNew.class);
         startActivity(cambioUs);
     }
 
-    public void vaciar(TextView tv){
+    public void vaciar(TextView tv) {
         tv.setText("");
     }
 
-    public void login (View view){
+    public void login(View view) {
+        //Vacia las cajas de texto
         TextInputEditText usuario = (TextInputEditText) findViewById(R.id.textName);
         TextInputEditText password = (TextInputEditText) findViewById(R.id.textPas);
-        usuario.getText().toString().replace("","");
-        password.getText().toString().replace("","");
 
-        if( usuario.getText().toString().equalsIgnoreCase("GOKU")&&
-        password.getText().toString().equals("123")){
-            Intent intent = new Intent (this, MainActivity.class);
-            startActivity(intent);
-        }else{
+        UserDbHelper mHelper = new UserDbHelper(this);
 
-            Toast toastUsu = Toast.makeText(this,"Comprueba nombre y contraseña", Toast.LENGTH_LONG);
+        SQLiteDatabase db2 = mHelper.getReadableDatabase();
+        String[] args = new String[]{usuario.getText().toString()};
+        //db.query("Usuarios", campos, "usuario=?", args, null, null, null);
+        Cursor cursor = db2.query(UsersContract.TaskEntry.TABLE,
+                new String[]{UsersContract.TaskEntry.NOMBRE_USUARIO, UsersContract.TaskEntry.PASSWORD_USUARIO},
+                "name=?", args, null, null, null);
+        // El metodo moveToNext() entra en funcionamiento cada vez que tu consulta o query, devuelve un resultado
+        // Esto significa que cuando pones GOku y no esta en base de datos, no va a hacer nada, ya que la logica de dentro solo actuara
+        // Cuando se devuelva algo de tu select
+
+        if (cursor.getCount() == 0) {
+            Toast toastUsu = Toast.makeText(this, "Comprueba nombre y contraseña", Toast.LENGTH_LONG);
             toastUsu.show();
+            usuario.getText().toString().replace("", "");
+            password.getText().toString().replace("", "");
         }
+        while (cursor.moveToNext()) {
 
+            String passwordParaHacerMatch = cursor.getString((Integer) cursor.getColumnIndex(UsersContract.TaskEntry.PASSWORD_USUARIO));
+
+            if (password.getText().toString().equals(passwordParaHacerMatch)) {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            } else {
+                Toast toastUsu = Toast.makeText(this, "Comprueba nombre y contraseña", Toast.LENGTH_LONG);
+                toastUsu.show();
+                usuario.getText().toString().replace("", "");
+                password.getText().toString().replace("", "");
+            }
+        }
+        cursor.close();
+        db2.close();
     }
 
     @Override
--- app/src/main/java/com/example/actividad2miguelangel/LoginNew.java
+++ app/src/main/java/com/example/actividad2miguelangel/LoginNew.java
@@ -2,27 +2,37 @@ package com.example.actividad2miguelangel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.actividad2miguelangel.TaskTable.TaskContract;
+import com.example.actividad2miguelangel.UsersTable.UsersContract;
+import com.example.actividad2miguelangel.UsersTable.UserDbHelper;
+
+import java.util.ArrayList;
 
 public class LoginNew extends AppCompatActivity {
 
     Button BtnCrear;
-
+    private UserDbHelper mHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_new);
-
-        BtnCrear = (Button) findViewById(R.id.buttonLogin);
-
+        BtnCrear = (Button) findViewById(R.id.botoncrear);
         final TextView textName = (TextView) findViewById(R.id.textName);
         final TextView textPassword = (TextView) findViewById(R.id.textPas);
 
@@ -53,6 +63,23 @@ public class LoginNew extends AppCompatActivity {
 
 
     public void Crear() {
+        mHelper = new UserDbHelper(this);
+        final TextView textoNombre = (TextView) findViewById(R.id.textName);
+        final TextView textoPassword = (TextView) findViewById(R.id.textPas);
+
+        //Instanciamos la base de datos con mHelper, y la hacemos escribible
+        //Creamos un "contenedor" que almacenara los valores que usaremos
+        //Creamos los campos para rellenar (USuario y contraseña)
+        //Insertamos los valores anteriores en TABLE = users, null , y los valores a procesar anteriores(Usuarios y password)
+        //Cerramos la base de datos
+        SQLiteDatabase db = mHelper.getWritableDatabase();
+        ContentValues valoresAProcesar = new ContentValues();
+        valoresAProcesar.put(UsersContract.TaskEntry.NOMBRE_USUARIO ,textoNombre.getText().toString());
+        valoresAProcesar.put(UsersContract.TaskEntry.PASSWORD_USUARIO, textoPassword.getText().toString());
+        db.insert(UsersContract.TaskEntry.TABLE, null, valoresAProcesar);
+        db.close();
+
+        //Reproducimos el Toast y el sonido
         LayoutInflater inflater = getLayoutInflater();
         View view = inflater.inflate(R.layout.toastnewusu, null);
         Toast toastNewUsu = new Toast (this);
@@ -65,14 +92,15 @@ public class LoginNew extends AppCompatActivity {
         Sonido1.start();
 
     }
-
+        //Aqui ponemos los campos vacios al doble click
     public void vaciar(TextView tv){
         tv.setText("");
     }
 
+
+    //La funcion de volver al menu anterior
     public void Cambio(View view) {
         Intent cambio = new Intent(this, Login.class);
         startActivity(cambio);
-
     }
 }
